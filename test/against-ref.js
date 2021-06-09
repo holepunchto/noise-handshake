066284d242130b784c78c1fe08051f6788dccd61
Christophe Diederichs <chris.d.itunes@gmail.com>
2021-06-09 14:25:06 +0100

    add interop test with reference impl

--- test/against-ref.js
+++ test/against-ref.js
@@ -1,5 +1,5 @@
 const ref = require('noise-protocol')
-const Noise = require('./noise')
+const Noise = require('../noise')
 const test = require('tape')
 
 test('XX handshake with reference impl', t => {
@@ -140,5 +140,44 @@ test('IK handshake with reference impl', t => {
   t.deepEqual(initiator.tx.key, splitClient.tx.subarray(0, 32))
   t.deepEqual(initiator.rx.key, splitServer.tx.subarray(0, 32))
   t.deepEqual(initiator.tx.key, splitServer.rx.subarray(0, 32))
+  t.end()  
+})
+
+test('handshake with reference impl', t => {
+  const initiator = new Noise('IK', true)
+  const responder = new Noise('IK', false)
+
+  const sServer = {
+    secretKey: responder.s.priv,
+    publicKey: responder.s.pub
+  }
+
+  initiator.initialise(Buffer.alloc(0), responder.s.pub)
+
+  const server = ref.initialize('IK', false, Buffer.alloc(0), sServer)
+  const serverRx = Buffer.alloc(512)
+  const serverTx = Buffer.alloc(512)
+
+  let splitClient
+
+  i = 0
+  while (!initiator.handshakeComplete) {
+    const message = initiator.send()
+    ref.readMessage(server, message, serverRx)
+
+    if (!splitClient) {
+      splitClient = ref.writeMessage(server, Buffer.alloc(0), serverTx)
+      initiator.recv(serverTx.subarray(0, ref.writeMessage.bytes))
+    }
+  }
+
+  // ->
+  // console.log('ref digest', responder.protocol.toString())
+  // // ->
+  // const splitClient = ref.writeMessage(client, Buffer.alloc(0), clientTx)
+  // const splitServer = ref.readMessage(server, clientTx.subarray(0, ref.writeMessage.bytes), serverRx)
+
+  t.deepEqual(initiator.rx.key, splitClient.rx.subarray(0, 32))
+  t.deepEqual(initiator.tx.key, splitClient.tx.subarray(0, 32))
   t.end()  
 })
