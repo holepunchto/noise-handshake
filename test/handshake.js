4d5e165989e1b716df85b8f355061bfd47d6763c
Christophe Diederichs <chris.d.itunes@gmail.com>
2021-09-08 17:09:30 +0100

    store remotePublicKey, clear ephemeral keys

--- test/handshake.js
+++ test/handshake.js
@@ -5,21 +5,30 @@ test('IK', t => {
   const initiator = new NoiseState('IK', true)
   const responder = new NoiseState('IK', false)
 
+  const rpk = new Uint8Array(responder.s.publicKey)
   initiator.initialise(Buffer.alloc(0), responder.s.publicKey)
   responder.initialise(Buffer.alloc(0))
 
-  while (!initiator.handshakeComplete) {
-    const message = initiator.send()
-    responder.recv(message)
+  const message = initiator.send()
+  responder.recv(message)
 
-    if (!responder.handshakeComplete) {
-      const reply = responder.send()
-      initiator.recv(reply)
-    }
-  }
+  const reply = responder.send()
+  initiator.recv(reply)
 
-  t.deepEqual(initiator.rx.key, responder.tx.key)
-  t.deepEqual(initiator.tx.key, responder.rx.key)
+  t.equal(initiator.key, null)
+  t.equal(initiator.nonce, null)
+  t.equal(initiator.curve, null)
+  t.equal(initiator.digest, null)
+  t.equal(initiator.chainingKey, null)
+  t.equal(initiator.offset, null)
+  t.equal(initiator.e, null)
+  t.equal(initiator.re, null)
+  t.equal(initiator.rs, null)
+
+  t.same(initiator.remotePublicKey, rpk)
+
+  t.deepEqual(initiator.rx, responder.tx)
+  t.deepEqual(initiator.tx, responder.rx)
   t.end()
 })
 
@@ -40,7 +49,7 @@ test('XX', t => {
     }
   }
 
-  t.deepEqual(initiator.rx.key, responder.tx.key)
-  t.deepEqual(initiator.tx.key, responder.rx.key)
+  t.deepEqual(initiator.rx, responder.tx)
+  t.deepEqual(initiator.tx, responder.rx)
   t.end()
 })
