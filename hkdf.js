1163c2acb49467571291b0baf0a859b1ac47463e
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-05-29 11:44:28 +0100

    avoid unnecessary loop

--- hkdf.js
+++ hkdf.js
@@ -1,5 +1,4 @@
 const hmacBlake2b = require('./hmac')
-const assert = require('nanoassert')
 const b4a = require('b4a')
 
 const HASHLEN = 64
@@ -14,39 +13,32 @@ module.exports = {
 
 function hkdf (salt, inputKeyMaterial, info = '', length = 2 * HASHLEN) {
   const pseudoRandomKey = hkdfExtract(salt, inputKeyMaterial)
-  const result = hkdfExpand(pseudoRandomKey, info, length)
+  return hkdfExpand(pseudoRandomKey, info, length)
+}
 
-  const results = []
-  let offset = 0
-  while (offset < result.length) {
-    results.push(result.subarray(offset, offset + HASHLEN))
-    offset += HASHLEN
+function hkdfExtract (salt, inputKeyMaterial) {
+  const hmac = b4a.alloc(HASHLEN)
+  return hmacDigest(hmac, salt, inputKeyMaterial)
+}
+
+function hkdfExpand (key, info, length) {
+  // Put in dedicated slab to avoid keeping shared slab from being gc'ed
+  const buffer = b4a.allocUnsafeSlow(length)
+
+  const infoBuf = b4a.from(info)
+  let prev = infoBuf
+
+  const result = []
+  for (let i = 0; i < length; i += HASHLEN) {
+    const pos = b4a.from([(i / HASHLEN) + 1])
+
+    const out = buffer.subarray(i, i + HASHLEN)
+    result.push(out)
+
+    prev = hmacDigest(out, key, [prev, infoBuf, pos])
   }
-  return results
 
-  function hkdfExtract (salt, inputKeyMaterial) {
-    const hmac = b4a.alloc(HASHLEN)
-    return hmacDigest(hmac, salt, inputKeyMaterial)
-  }
-
-  function hkdfExpand (key, info, length) {
-    // Put in dedicated slab to avoid keeping shared slab from being gc'ed
-    const result = b4a.allocUnsafeSlow(length)
-
-    const infoBuf = b4a.from(info)
-    let prev = infoBuf
-
-    for (let i = 0; i < length; i += HASHLEN) {
-      const out = result.subarray(i, i + HASHLEN)
-      const pos = b4a.from([(i / HASHLEN) + 1])
-
-      prev = hmacDigest(out, key, [prev, infoBuf, pos])
-    }
-
-    assert(result.byteLength === length, 'key expansion failed, length not as expected')
-
-    return result
-  }
+  return result
 }
 
 function hmacDigest (out, key, input) {
